b857d2a756d41b6199ef2ad5e481e061709d07d9
flacode <flacode@github.com>
2018-08-09 13:02:49 +0300

    [Feature #159561794] Add user sign up feature - Add model for users. - Add account activation feature.  - Add tests for user sign up.

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    notification_time = models.TimeField(null=True, blank=True)
+    slug_field = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.email
